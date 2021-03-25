f11ee2f0e6400d3108108e4fe148b4aaf57e8532
taylorsieling <taylorasieling@gmail.com>
2021-03-25 18:22:25 -0500

    commit tree 503b9cfa9323d7fb1cf128e6a61bf72706455ed6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
+
+let brunch = new Breakfast("Eggs Benedict", "Mimosa")
+let classic = new Lunch("House Salad", "French Onion Soup", "Iced Tea")
+let fancy = new Dinner("Caesar", "Tomato Basil", "Eggplan Paremsean", "Cheesecake")
